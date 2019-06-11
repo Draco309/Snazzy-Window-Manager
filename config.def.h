74096b0491d0050fa75850f39f114ed589b7ff33
Sammi Turner <ms.sammi.turner@gmail.com>
2019-06-11 10:05:20 +0100

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,26 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 16;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 16;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 21;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 21;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 21;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 21;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#282a36";
-static const char col_gray2[]       = "#fefefe";
-static const char col_gray3[]       = "#fefefe";
-static const char col_gray4[]       = "#fefefe";
-static const char col_cyan[]        = "#282a36";
+static const char col_gray1[]       = "#282828";
+static const char col_gray2[]       = "#f68516";
+static const char col_gray3[]       = "#f68516";
+static const char col_gray4[]       = "#f68516";
+static const char col_cyan[]        = "#282828";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray3, col_gray1, col_gray2  },
 };
 
 /* tagging */
